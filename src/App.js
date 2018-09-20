7f82cb3247cb501513e1e3b585486b35c88966c5
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-19 19:08:05 -0600

    Built skelton, components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//Components
+import Dashboard from './component/Dashboard/Dashboard.js';
+import Header from './component/Header/Header';
+import Wizard from './component/Wizard/Wizard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        < Dashboard />
+        < Wizard />
+        < Header />
+
       </div>
     );
   }
---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import House from '../House/House';
+
+class Dashboard extends Component{
+    render(){
+        return(
+            <div>
+                <div>Dashboard</div>
+                < House />
+            </div>
+        )
+    }
+}
+export default Dashboard;
